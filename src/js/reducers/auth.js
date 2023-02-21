b5294c509e1f2a5729089367bea030b8118bc994
Priscila da Silva Ferreira <mahteusodev@gmail.com>
2023-02-21 11:22:03 -0300

    error handling

--- src/js/reducers/auth.js
+++ src/js/reducers/auth.js
@@ -1,35 +1,62 @@
 import { combineReducers } from "redux";
 
+function loginReducer(state = { error: null }, action) {
+  switch (action.type) {
+    case 'AUTH_LOGIN_INIT':
+      return { error: null };
+    case 'AUTH_LOGIN_ERROR':
+      return { error: action.error };
+    default:
+      return state;
+  }
+}
+
+function registerReducer(state = { error: null }, action) {
+  switch (action.type) {
+    case 'AUTH_REGISTER_INIT':
+      return { error: null };
+    case 'AUTH_REGISTER_ERROR':
+      return { error: action.error };
+    default:
+      return state;
+  }
+}
+
 function createAuthReducer() {
-    const user = (state = null, action) => {
-        switch (action.type) {
-            case 'AUTH_ON_ERROR':
-            case 'AUTH_ON_INIT':
-                return null;
-            case 'AUTH_ON_SUCCESS':
-                return action.user;
-            default:
-                return state;
-        }
+  const user = (state = null, action) => {
+    switch (action.type) {
+      case 'AUTH_ON_ERROR':
+      case 'AUTH_ON_INIT':
+        return null;
+      case 'AUTH_ON_SUCCESS':
+        return action.user;
+      default:
+        return state;
     }
+  }
 
-    const isChecking = (state = false, action) => {
-        switch (action.type) {
-            case 'AUTH_ON_INIT':
-            case 'AUTH_LOGIN_INIT':
-            case 'AUTH_REGISTER_INIT':
-                return true;
-            case 'AUTH_ON_ERROR':
-            case 'AUTH_ON_SUCCESS':
-                return false;
-            default:
-                return state;
-        }
+  const isChecking = (state = false, action) => {
+    switch (action.type) {
+      case 'AUTH_ON_INIT':
+      case 'AUTH_LOGIN_INIT':
+      case 'AUTH_REGISTER_INIT':
+        return true;
+      case 'AUTH_ON_ERROR':
+      case 'AUTH_ON_SUCCESS':
+      case 'AUTH_REGISTER_SUCCESS':
+      case 'AUTH_LOGIN_SUCCESS':
+        return false;
+      default:
+        return state;
     }
+  }
 
-    return combineReducers({
-        user, isChecking
-    })
+  return combineReducers({
+    user,
+    isChecking,
+    login: loginReducer,
+    register: registerReducer
+  })
 }
 
 export default createAuthReducer();
